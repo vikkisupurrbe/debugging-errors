2933a8e456c7c28d0a4abb955faa6c1a453587f4
Vikki <yingjialiu1216@gmail.com>
2025-02-18 23:33:51 -0800

    corrected typo, corrected reverse function

--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
-var input = process.argv[2];
+let input = process.argv[2];
 
 if (input)
   console.log(reverse(input));
 
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  return original.split('').reverse().join('');
 }
